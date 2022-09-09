6afe10051be60a53e594760dbe57d406315da02a
Maria Emanuilova <mariyakoseva@yahoo.com>
2022-09-09 14:50:41 +0300

    adding checkbox and radio button selection exercise

---
+++ first.js
@@ -0,0 +1,12 @@
+const {chromium} = require("playwright");
+
+(async() => {
+//launchihng browser
+const browser = await chromium.launch({headless: false, slowMo:100});
+//creating a page inside the browser
+const page = await browser.newPage();
+//navigating to site
+await page.goto();
+//closing the browser
+await browser.close();
+})
